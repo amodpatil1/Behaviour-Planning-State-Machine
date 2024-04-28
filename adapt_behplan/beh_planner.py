e34a3151425421876e8e0bc21e0d71f67fb5a2aa
rit3148s <rit3148s@hs-coburg.de>
2024-04-28 19:46:24 +0200

    updated code

--- adapt_behplan/beh_planner.py
+++ adapt_behplan/beh_planner.py
@@ -31,7 +31,7 @@ class CarBehaviour(Node):
         self.angular_vel = 0.0
 
     def twist_callback(self):
-        if self.waypoints:
+        if self.waypoints and self.reached_parking_spot == False:
             msg = Twist()
 
             vehicle_stat = self.vicinity_check()
@@ -43,14 +43,16 @@ class CarBehaviour(Node):
                 msg.linear.x = 0.001
                 msg.angular.z = self.steering_calc()
                 self.get_logger().info('linear vel: %f, angular vel: %f' % (msg.linear.x, msg.linear.y))
-
+            self.cmd_vel.publish(msg)    
+        self.waypoint_increment()  
+        
     def route_callback(self, msg):
+
         self.waypoints = msg.poses
         self.get_logger().info('received waypoints')
         self.curr_way_idx = 0
 
-    def veh_loc(self, msg):
-
+    def veh_loc(self, msg):     
         self.curr_loc = (round(msg.pose.position.x, 4), round(msg.pose.position.y, 4))
         self.get_logger().info('location of vehicle: (%f, %f)' % (self.curr_loc[0], self.curr_loc[1]))
 
@@ -58,48 +60,57 @@ class CarBehaviour(Node):
         self.get_logger().info('final destination is : (%f, %f)' % (self.final_loc[0], self.final_loc[1]))
 
     def rotation_callback(self, msg):
+        
         self.curr_yaw = math.degrees(msg.z)
         self.get_logger().info('yaw angle : %f' % (self.curr_yaw))
 
     def steering_calc(self):
-        if self.curr_way_idx < len(self.waypoints):
+        if self.curr_way_idx < len(self.waypoints)-1:
 
-            self.next_waypoint = (self.waypoints[self.curr_way_idx + 1].position.x, self.waypoints[self.curr_way_idx + 1].position.y)
+            #self.next_waypoint = (self.waypoints[self.curr_way_idx + 1].position.x, self.waypoints[self.curr_way_idx + 1].position.y)
 
-            if self.curr_yaw == -90:
+            if self.curr_yaw == -90.0:
                 self.get_logger().info('no deviation')
 
             else:   
-                if 0 < self.curr_yaw < -90:
-                    self.dev_angle = 90 - abs(self.curr_yaw)
+                if 0.0 < self.curr_yaw < -90.0:
+                    self.dev_angle = 90.0 - abs(self.curr_yaw)
                     self.angular_vel =  float(self.dev_angle * 0.23)
                     self.get_logger().info('steering angle: %f' % (self.angular_vel))
+                    return float(self.angular_vel)
 
-                if -90 < self.curr_yaw < -180:
-                    self.dev_angle = abs(self.curr_yaw) - 90
+                if -90.0 < self.curr_yaw < -180.0:
+                    self.dev_angle = abs(self.curr_yaw) - 90.0
                     self.angular_vel = float(-self.dev_angle * 0.23)
                     self.get_logger().info('steering angle: %f' % (self.angular_vel))
+                    return float(self.angular_vel) 
+            return 0.0
 
-        return self.angular_vel      
   
     def vicinity_check(self):
         if 3 < self.curr_loc[0] < 4:
+            self.get_logger().info('the vehicle is on track')
             return 'on_route' 
         else:
+            self.get_logger().info('the vehicle is off track')
             return 'off route'
         
     def waypoint_increment(self):
+        if self.waypoints:
+            if self.curr_way_idx == len(self.waypoints)-1:
+                self.reached_parking_spot = True
 
-        if self.curr_loc[1] < self.waypoints[self.curr_way_idx + 1].position.y:
-           self.get_logger().info('the vehicle is approaching the next waypoint')
+            else:
+                if self.curr_loc[1] < self.waypoints[self.curr_way_idx + 1].position.y:
+                    self.get_logger().info('the vehicle is approaching the next waypoint')
   
-        elif self.curr_loc[1] >= self.waypoints[self.curr_way_idx + 1].position.y:
-             self.get_logger().info('the waypoint (%f,%f) has been reached' % (self.waypoints[self.curr_way_idx + 1].position.x,
+                elif self.curr_loc[1] >= self.waypoints[self.curr_way_idx + 1].position.y:
+                    self.get_logger().info('the waypoint (%f,%f) has been reached' % (self.waypoints[self.curr_way_idx + 1].position.x,
                                                                              self.waypoints[self.curr_way_idx + 1].position.y))
-             self.curr_way_idx += 1
-             return self.curr_way_idx
-        else:
-            self.get_logger().error('something went wrong')
+                    self.curr_way_idx += 1
+                    print(self.curr_way_idx)
+                else:
+                    self.get_logger().error('something went wrong')
         
 
 def main(args=None):
